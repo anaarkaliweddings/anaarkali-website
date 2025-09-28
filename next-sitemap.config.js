bb1e30fae88c17fe741b4a4e3311d6c33b566d07
raeskaa <maheshtripathidec@gmail.com>
2025-09-28 19:25:50 +0530

    Fix sitemap URLs to use www version for Google Search Console

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://anaarkaliproduction.com',
+  siteUrl: 'https://www.anaarkaliproduction.com',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'weekly',
